14bc7ee62e7042c795760c17f8710007cc535164
Jyoti Lama <97466232+JLama75@users.noreply.github.com>
2024-10-15 15:29:35 -0400

    Update slurm.submit.step9.sh

--- slurm.submit.step9.sh
+++ slurm.submit.step9.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sbatch step9.submit.sh list_annot_run1 Run1_Count  
-sbatch step9.submit.sh list_annot_run2 Run2_Count  
-sbatch step9.submit.sh list_annot_run3 Run3_Count  
-sbatch step9.submit.sh list_annot_run4 Run4_Count  
+sbatch step9.submit.sh "list_annot_run1" Run1_Count  
+sbatch step9.submit.sh "list_annot_run2" Run2_Count  
+sbatch step9.submit.sh "list_annot_run3" Run3_Count  
+sbatch step9.submit.sh "list_annot_run4" Run4_Count 
