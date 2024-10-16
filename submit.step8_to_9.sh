b82118f6c747d74c4d211bcc2bd17aed34a4de50
Jyoti Lama <97466232+JLama75@users.noreply.github.com>
2024-10-16 11:13:28 -0400

    Update submit.step8_to_9.sh

--- submit.step8_to_9.sh
+++ submit.step8_to_9.sh
@@ -6,10 +6,11 @@
 #SBATCH --output=Step8_to_9_101524_%j.log   # Standard output and error log
 #SBATCH --partition=medium,long,short
 
-export RegeniefileName='SIOP_FAME.ALL.101524_responder'
+export RegeniefileName='SIOP_FAME.ALL.101524_responder' #Change to your Regenie file name
+
 echo -e "running step8 ..."
-python step8.py --regenie './step7/'${RegeniefileName}'.regenie' --outFile './step8_to_10/'${RegeniefileName}'.tsv'
+python step8.py --regenie ${RegeniefileName} 
 
 echo -e "running step9..."
-./slurm.step9.sh --vcf './step1/FAME.TopSignif_ENSEMBL_withNEAREST.100724.vcf' --RegeniefileName 'SIOP_FAME.ALL.101524_responder' 
-
+#Enter --vcf path_to_your_annotated_vcf 
+./slurm.step9.sh --vcf './step1/FAME.TopSignif_ENSEMBL_withNEAREST.100724.vcf' --RegeniefileName ${RegeniefileName} 
