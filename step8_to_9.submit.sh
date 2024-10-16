5d15155d76d7aee14d088160802ff44be8fb37aa
Jyoti Lama <97466232+JLama75@users.noreply.github.com>
2024-10-15 20:58:50 -0400

    Update and rename step8.submit.sh to step8_to_9.submit.sh

--- step8_to_9.submit.sh
+++ step8_to_9.submit.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=step8   # Job name
+#SBATCH --job-name=step8_to_9   # Job name
 #SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=jlama@meei.harvard.edu     # Where to send mail    
 #SBATCH --mem=1G --nodes=1              # Job memory request
-#SBATCH --output=Step8_%j.log   # Standard output and error log
+#SBATCH --output=Step8_to_9_101524_%j.log   # Standard output and error log
 #SBATCH --partition=medium,long,short
 
 export RegeniefileName='SIOP_FAME.ALL.101524_responder'
-
+echo -e "running step8 ..."
 python step8.py --regenie './step7/'${RegeniefileName}'.regenie' --outFile './step8_to_10/'${RegeniefileName}'.tsv'
+
+echo -e "running step9..."
+./slurm.submit.step9.sh --vcf './step1/FAME.TopSignif_ENSEMBL_withNEAREST.100724.vcf' --RegeniefileName 'SIOP_FAME.ALL.101524_responder' 
 
