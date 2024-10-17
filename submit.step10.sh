5812cc392b4fd167709c682ca2cd6bf6e80b547c
Jyoti Lama <97466232+JLama75@users.noreply.github.com>
2024-10-17 18:37:56 -0400

    Update submit.step10.sh

--- submit.step10.sh
+++ submit.step10.sh
@@ -3,8 +3,10 @@
 #SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=jlama@meei.harvard.edu     # Where to send mail    
 #SBATCH --mem=1G --nodes=1              # Job memory request
-#SBATCH --output=Step10_101524_%j.log   # Standard output and error log
+#SBATCH --output=Step10.2_101524_%j.log   # Standard output and error log
 #SBATCH --partition=medium,long,short
 
 echo -e "running step10..."
-python step10.py
+#dir=$1
+dir=dir='/data/Segre_Lab/users/jlama/WES_new.ALL_050824/GeneBurden/FAME_updated/step8_to_10'
+python step10.py --Dir ${dir}
