1a67cee418876bdb097784b8f7c983432bc1eab4
Jyoti Lama <97466232+JLama75@users.noreply.github.com>
2024-11-12 14:10:32 -0500

    Update slurm.step9.sh

--- slurm.step9.sh
+++ slurm.step9.sh
@@ -3,25 +3,28 @@
 # Initialize variables
 vcf=""
 RegeniefileName=""
+outputDir=""
 
 # Parse named arguments
 while [[ "$#" -gt 0 ]]; do
     case $1 in
-        --vcf) vcf="$2"; shift ;;        # Get the value for --vcf
+        --vcf) vcf="$2"; shift ;;  # Get the value for --vcf
         --RegeniefileName) RegeniefileName="$2"; shift ;; # Get the value for --RegeniefileName
-        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+        --outputDir) outputDir="$2"; shift ;; # Get the value for --outputDir
+        *) echo "Unknown parameter passed: $1"; exit 1 ;; # Catch unknown parameters
     esac
     shift
 done
 
 # Check if the required arguments are provided
-if [[ -z "$vcf" || -z "$RegeniefileName" ]]; then
-    echo "Error: --vcf and --RegeniefileName are required."
+if [[ -z "$vcf" || -z "$RegeniefileName" || -z "$outputDir" ]]; then
+    echo "Error: --vcf, --RegeniefileName, and --outputDir are required."
     exit 1
 fi
 
+
 # Submit jobs with different annotations, passing vcf and filename as arguments
-sbatch step9.sh "list_annot_run1" Run1_Count  "$vcf" "$RegeniefileName"
-sbatch step9.sh "list_annot_run2" Run2_Count  "$vcf" "$RegeniefileName"
-sbatch step9.sh "list_annot_run3" Run3_Count  "$vcf" "$RegeniefileName"
-sbatch step9.sh "list_annot_run4" Run4_Count  "$vcf" "$RegeniefileName"
+sbatch step9.sh "list_annot_run1" Run1_Count  "$vcf" "$RegeniefileName" "$outputDir"
+sbatch step9.sh "list_annot_run2" Run2_Count  "$vcf" "$RegeniefileName" "$outputDir"
+sbatch step9.sh "list_annot_run3" Run3_Count  "$vcf" "$RegeniefileName" "$outputDir"
+sbatch step9.sh "list_annot_run4" Run4_Count  "$vcf" "$RegeniefileName" "$outputDir"
